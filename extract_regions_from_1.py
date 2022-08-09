996ff8d90df2741a3057176bbe772d7e48cbce61
larsaars <lurzapps@gmail.com>
2022-08-09 19:22:23 +0200

    regions

--- extract_regions_from_1.py
+++ extract_regions_from_1.py
@@ -5,42 +5,44 @@
 Extract region from the GeoJSON region files in folder '1'.
 Put them into 'regions' folder with county subfolders with the same name as their region.
 In these subfolders also put a file with the list of regions ('regions.txt').
-
-Finally, scale down the GeoJSON to 25% with mapshaper.
 """
 
 import json
 import os
+import re
 
 def main():
     for file in os.listdir('./1'):
-        with open('./1/' + file) as f:
-            data = json.load(f)
+        with open('./1/' + file) as original_region_file:
+            data = json.load(original_region_file)
 
             country_name = file.split('.')[0]
             
             if not os.path.exists(f'./regions/{country_name}'):
                 os.mkdir(f'./regions/{country_name}')
 
-            regions = []
+            regions_names = []
 
             for feature in data['features']:
-                region_name = feature['properties']['NAME_1']
-                regions.append(region_name)
+                # for keyword search make sure to use lowercase
+                # and add space in font of every uppercase word
+                region_name = str(feature['properties']['NAME_1'])
+                region_name = re.sub(r'\W+', '', region_name)
+                region_name = re.sub(r"(\w)([A-Z])", r"\1 \2", region_name).strip().lower()
 
-                with open(f'regions/{country_name}/{region_name}.json', 'w') as f:
-                    json.dump(feature, f)
+                regions_names.append(region_name)
+
+                region_name_file = region_name.replace(' ', '_')
+                with open(f'regions/{country_name}/{region_name_file}.json', 'w') as region_file:
+                    json.dump(feature, region_file)
 
 
-            with open(f'./regions/{country_name}/regions.txt', 'w') as f:
-                json.dump('\n'.join(regions), f)
+            with open(f'./regions/{country_name}/regions.txt', 'w') as regions_list_file:
+                regions_list_file.write('\n'.join(regions_names))
 
 
             print(f'Regions from {country_name} extracted.')
 
-            os.system(f'mapshaper ./regions/{country_name}/*.json -simplify dp 25% keep-shapes -o force format=geojson')
-
-            print('GeoJSONs scaled down for {country_name}.')
 
 
 if __name__ == '__main__':
